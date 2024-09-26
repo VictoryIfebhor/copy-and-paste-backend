17b85169468362ca016ad8e0da4f20a6159e38e3
Victory Ifebhor <63762763+VictoryIfebhor@users.noreply.github.com>
2024-09-26 18:16:40 +0100

    Update user.router.js
    Include login with password in the router

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   authenticateUser,
+  loginUserWithEmailPassword,
   currentUserInfo,
 } from "../controllers/user.controller.js";
 import { authMiddleware } from "../middlewares/auth-handler.js";
@@ -8,6 +9,7 @@ import { authMiddleware } from "../middlewares/auth-handler.js";
 const router = express.Router();
 
 router.post("/auth", authenticateUser);
+router.post("/login", loginUserWithEmailPassword);
 router.get("/me", authMiddleware, currentUserInfo);
 
 export default router;
